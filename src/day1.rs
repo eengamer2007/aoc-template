334ee1b1ba33a7e0c903b4917f48a6da66d152a8
eengamer2007 <61841788+eengamer2007@users.noreply.github.com>
2021-11-30 09:26:16 +0100

    initial commit

---
+++ src/day1.rs
@@ -0,0 +1,16 @@
+/*
+#[aoc_generator(day1)]
+pub fn part2(input: &str) -> i32 {
+    0
+}
+*/
+
+#[aoc(day1, part1)]
+pub fn part1(input: &str) -> i32 {
+    0
+}
+
+#[aoc(day1, part2)]
+pub fn part2(input: &str) -> i32 {
+    0
+}
---
+++ src/main.rs
@@ -0,0 +1,7 @@
+extern crate __;
+extern crate aoc_runner;
+#[macro_use]
+extern crate aoc_runner_derive;
+
+
+aoc_main! { lib = __}
